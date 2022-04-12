9e012d267bef0591797e2a2742c49f58a1a826ff
Zhong Yao <zhongyao0799@qq.com>
2022-04-13 04:19:43 +0800

    feat(ui): simple story of AutoComplete

--- libs/ui/src/lib/__stories__/AutoComplete.stories.tsx
+++ libs/ui/src/lib/__stories__/AutoComplete.stories.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { AutoComplete } from '../AutoComplete';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
@@ -6,17 +7,28 @@ export default {
   component: AutoComplete,
 } as ComponentMeta<typeof AutoComplete>;
 
-const mockOptions = [
-  { label: 'haah', value: 'haah' },
-  { label: 'foo', value: 'foo' },
-  { label: 'bar', value: 'bar' },
-];
+interface Option {
+  label: string;
+  value: string;
+}
 
-export const Base: ComponentStory<typeof AutoComplete> = () => (
-  <AutoComplete
-    options={mockOptions}
-    onSearch={(value) => {
-      console.log(value);
-    }}
-  />
-);
+export const Base: ComponentStory<typeof AutoComplete> = () => {
+  const [options, setOptions] = useState<Option[]>([]);
+
+  const mockSearchOptions = (value: string) => {
+    if (!value) {
+      setOptions([]);
+      return;
+    }
+    const newOptions = new Array(3).fill(0).map((_, index) => {
+      let ret = '';
+      for (let i = 0; i <= index; i++) {
+        ret += value;
+      }
+      return { label: ret, value: ret };
+    });
+    setOptions(newOptions);
+  };
+
+  return <AutoComplete options={options} onSearch={mockSearchOptions} />;
+};
