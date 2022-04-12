4b5b6461ae4d68b0fb59ce656a48d56c3818d16c
Zhong Yao <zhongyao0799@qq.com>
2022-04-12 10:21:18 +0800

    feat(ui): add component Popper

---
+++ libs/ui/src/lib/Popper/Popper.tsx
@@ -0,0 +1,54 @@
+import { forwardRef, ReactNode, useCallback, useState } from 'react';
+import { usePopper } from 'react-popper';
+import { Portal } from '../Portal';
+import type { Placement } from '@popperjs/core';
+
+export interface PopperProps {
+  anchorEl: Element | null;
+  open?: boolean;
+  getPopupContainer?: () => Element;
+  placement?: Placement;
+  children?: ReactNode;
+}
+
+const PopperTooltip = forwardRef<
+  Element,
+  Omit<PopperProps, 'getPopupContainer'>
+>((props, ref) => {
+  const { children, placement, anchorEl, open } = props;
+
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(
+    null
+  );
+  const popperElementCallbackRef = useCallback(
+    (el: HTMLDivElement | null) => setPopperElement(el),
+    []
+  );
+
+  const popper = usePopper(anchorEl, open ? popperElement : null, {
+    placement,
+  });
+
+  return (
+    <div
+      ref={popperElementCallbackRef}
+      style={popper.styles['popper']}
+      role="tooltip"
+      {...popper.attributes['popper']}
+    >
+      {children}
+    </div>
+  );
+});
+
+export const Popper = forwardRef<Element, PopperProps>((props, ref) => {
+  const { getPopupContainer, children, ...tooltipProps } = props;
+
+  const container = getPopupContainer?.() || document.body;
+
+  return (
+    <Portal container={container}>
+      <PopperTooltip {...tooltipProps}>{children}</PopperTooltip>
+    </Portal>
+  );
+});
